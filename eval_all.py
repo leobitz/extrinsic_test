6dae4e303dbbdbbe614b5ff4cc22e234e7b26f09
Leo Bitz <amanyamy@gmail.com>
2020-04-09 15:01:31 +0300

    eval iteration fix

--- eval_all.py
+++ eval_all.py
@@ -9,9 +9,10 @@ for embed_size in [100, 200, 300]:
     for run in [4, 8, 12]:
         for win_size in [1, 3, 5]:
             for char_size in [5, 6]:
-                for iter in [1]:
+                for iter in [1, 2, 3, 4, 5]:
                     vector_file="vectors/fasttext/{0}-{1}-{2}-{3}-{4}.vec".format(embed_size, run, win_size, char_size, iter)
                     if os.path.exists(vector_file):
+                        print(vector_file)
                         result = evaluate.evaluate(vector_file, eval_file) # evaluate
                         line = [embed_size, run, win_size, iter, result['semantic'], result['syntactic'], result['total']]
                         line = ",".join([str(x) for x in line])
