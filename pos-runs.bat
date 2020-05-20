89ea805767a2472380a6633674fe9c7b5fc71d9d
Leo Bitz <amanyamy@gmail.com>
2020-05-20 22:00:07 +0300

    pos runner

--- pos-runs.bat
+++ pos-runs.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
-for %%h in (128) do (
+for %%h in (256) do (
 	for %%b in (32) do (
-		for %%c in (small mid big) do (
-			for %%v in (scratch fasttext fasttext-alpha) do (
-                for %%t in (0 1) do (
-					for %%r in (1 2 3 4 5) do (
+		for %%c in (small) do (
+			for %%v in (fasttext) do (
+                for %%t in (1) do (
+					for %%r in (1) do (
 						if exist result\pos\%%c-%%v-%%h-%%t-%%r-9 (
 							echo file exists
 						) else (
 							echo result\pos\%%c-%%v-%%h-%%t-%%r
-							python .\pos-tagger.py --corpus %%c --vector %%v -u %%h -e 10 -b %%b -s 62 -t %%t -r %%r
+							python .\pos-taggerD.py --corpus %%c --vector %%v -u %%h -e 15 -b %%b -s 70 -t %%t -r %%r
 						)
 					)
 				)
