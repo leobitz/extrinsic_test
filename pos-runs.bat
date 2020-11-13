92456062cca03043ace0cb9503b3bc60d2501a1b
Leo Bitz <amanyamy@gmail.com>
2020-11-14 01:19:33 +0300

    run

--- pos-runs.bat
+++ pos-runs.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
-for %%h in (512) do (
+for %%h in (256 512) do (
 	for %%b in (16) do (
-		for %%c in (big) do (
-			for %%v in (fasttext) do (
+		for %%c in (small mid big) do (
+			for %%v in (word2vec fasttext fasttext-alpha) do (
                 for %%t in (1) do (
-					for %%r in (1) do (
+					for %%r in (1 2 3 4 5) do (
 						if exist result\pos\%%c-%%v-%%h-%%t-%%r-9 (
 							echo file exists
 						) else (
